e9f6b1f1ca90c12d40d408127451002ded792944
grytole <grytole@gmail.com>
2024-01-23 20:58:54 +0200

    initial commit

--- crsf.c
+++ crsf.c
@@ -1,4 +1,4 @@
-/* crsf implementation for usart (stm32f103c8t6) */
+/* crsf implementation for hardware usart (stm32f103c8t6) */
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
@@ -6,171 +6,139 @@
 #include <libopencm3/cm3/nvic.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include "ring.h"
 #include "crc8.h"
 #include "crsf.h"
 
-
-/* defines */
+#define CRSF_USART (USART3)
 
 #define CRSF_BAUDRATE       (420000)
 #define CRSF_CRC_POLY       (0xD5)
 #define CRSF_MAX_PACKET_LEN (64)
-#define CRSF_POS_DEST       (0)
-#define CRSF_POS_LEN        (1)
-#define CRSF_POS_TYPE       (2)
-#define CRSF_POS_PAYLOAD    (3)
-#define CRSF_DEST_FC        (0xC8)
-#define CRSF_TYPE_CHANNELS  (0x16)
-#define CRSF_TYPE_HEARTBEAT (0x0B)
-#define CRSF_TYPE_TEL_BATT  (0x08)
-#define CRSF_LEN_CHANNELS   (22)
-#define CRSF_LEN_HEARTBEAT  (2)
-#define CRSF_LEN_TEL_BATT   (8)
+#define CRSF_SYNC           (0xC8)
+
+#define CRSF_BUFFER_POS_SYNC     (0)
+#define CRSF_BUFFER_POS_LEN      (1)
+#define CRSF_BUFFER_POS_TYPE     (2)
+#define CRSF_BUFFER_POS_PAYLOAD  (3)
+
+#define CRSF_TYPE_RC_CHANNELS_PACKED (0x16)
+
+#define CRSF_LEN_RC_CHANNELS_PACKED  (22)
+
 #define CRSF_FAILSAFE_TIMEOUT_MS (100)
 
-
-/* typedefs */
-
-typedef struct __attribute__((packed)) {
-  bool initialized;
-  uint32_t usart;
-  uint32_t gpio_port_rx;
-  uint32_t gpio_port_tx;
-  uint16_t gpio_pin_rx;
-  uint16_t gpio_pin_tx;
-  uint8_t irq;
-  Ring rx_ring;
-  Ring tx_ring;
-} crsf_config_t;
-
-typedef union {
-  struct __attribute__((packed)) raw {
-    uint8_t type;
-    uint8_t data[61];
-  };
-  /* type: 0x02 */
-  struct __attribute__((packed)) gps {
-    uint8_t type;
-    int32_t lat;
-    int32_t lon;
-    int16_t speed;
-    int16_t course;
-    uint16_t alt;
-    uint8_t sat_count;
-    uint8_t crc;
-  };
-  /* type: 0x08 */
-  struct __attribute__((packed)) battery_sensor {
-    uint8_t type;
-    int16_t voltage;
-    int16_t current;
-    int32_t used_capacity : 24;
-    int8_t battery_remaining;
-    uint8_t crc;
-  };
-  /* type: 0x09 */
-  struct __attribute__((packed)) baro_altitude {
-    uint8_t type;
-    uint16_t altitude;
-    uint8_t crc;
-  };
-  /* type: 0x09 (alt) */
-  struct __attribute__((packed)) baro_altitude_vario {
-    uint8_t type;
-    uint16_t altitude;
-    int16_t vertical_speed;
-    uint8_t crc;
-  };
-  /* type: 0x0B */
-  struct __attribute__((packed)) heartbeat {
-    uint8_t type;
-    uint16_t origin_device_address;
-    uint8_t crc;
-  };
-  /* type: 0x16 */
-  struct __attribute__((packed)) rc_channels_packed {
-    uint8_t type;
-    uint16_t channels[16] : 11;
-    uint8_t crc;
-  };
-  /* type: 0x1E */
-  struct __attribute__((packed)) attitude {
-    uint8_t type;
-    int16_t pitch;
-    int16_t roll;
-    int16_t yaw;
-    uint8_t crc;
-  };
-  /* type: 0x21 (TODO) */
-  struct __attribute__((packed)) flight_mode {
-    uint8_t type;
-    uint8_t mode_string[14];
-    uint8_t crc;
-  };
-} crsf_payload_u;
+#if (CRSF_USART == USART1)
+ #define CRSF_USART_RCC    (RCC_USART1)
+ #define CRSF_GPIO_PORT_RX (GPIO_BANK_USART1_RX)
+ #define CRSF_GPIO_PORT_TX (GPIO_BANK_USART1_TX)
+ #define CRSF_GPIO_PIN_RX  (GPIO_USART1_RX)
+ #define CRSF_GPIO_PIN_TX  (GPIO_USART1_TX)
+ #define CRSF_USART_ISR    (usart1_isr)
+ #define CRSF_USART_IRQ    (NVIC_USART1_IRQ)
+#elif (CRSF_USART == USART2)
+ #define CRSF_USART_RCC    (RCC_USART2)
+ #define CRSF_GPIO_PORT_RX (GPIO_BANK_USART2_RX)
+ #define CRSF_GPIO_PORT_TX (GPIO_BANK_USART2_TX)
+ #define CRSF_GPIO_PIN_RX  (GPIO_USART2_RX)
+ #define CRSF_GPIO_PIN_TX  (GPIO_USART2_TX)
+ #define CRSF_USART_ISR    (usart2_isr)
+ #define CRSF_USART_IRQ    (NVIC_USART2_IRQ)
+#elif (CRSF_USART == USART3)
+ #define CRSF_USART_RCC    (RCC_USART3)
+ #define CRSF_GPIO_PORT_RX (GPIO_BANK_USART3_RX)
+ #define CRSF_GPIO_PORT_TX (GPIO_BANK_USART3_TX)
+ #define CRSF_GPIO_PIN_RX  (GPIO_USART3_RX)
+ #define CRSF_GPIO_PIN_TX  (GPIO_USART3_TX)
+ #define CRSF_USART_ISR    (usart3_isr)
+ #define CRSF_USART_IRQ    (NVIC_USART3_IRQ)
+#else
+ #error Invalid USART device selected
+#endif
 
 typedef struct __attribute__((packed)) {
-  uint8_t sync;
-  uint8_t len;
-  crsf_payload_u payload;
-} crsf_packet_t;
+  uint16_t ch0 : 11;
+  uint16_t ch1 : 11;
+  uint16_t ch2 : 11;
+  uint16_t ch3 : 11;
+  uint16_t ch4 : 11;
+  uint16_t ch5 : 11;
+  uint16_t ch6 : 11;
+  uint16_t ch7 : 11;
+  uint16_t ch8 : 11;
+  uint16_t ch9 : 11;
+  uint16_t ch10 : 11;
+  uint16_t ch11 : 11;
+  uint16_t ch12 : 11;
+  uint16_t ch13 : 11;
+  uint16_t ch14 : 11;
+  uint16_t ch15 : 11;
+} crsf_rc_channels_t;
 
+static uint8_t rx_buf[255] = {};
+static uint8_t tx_buf[255] = {};
+static Ring rx_ring;
+static Ring tx_ring;
+static uint8_t crsf_buffer[CRSF_MAX_PACKET_LEN] = {};
+static uint8_t crsf_buffer_pos = 0;
+static uint16_t crsf_rc_channels[16] = {};
 
-/* variables */
-static uint8_t rx_buf[256] = {};
-static uint8_t tx_buf[256] = {};
+static bool crsf_parse_input(void);
+static void crsf_parse_packet(uint8_t type, uint8_t *payload, uint8_t len);
+static void crsf_reset_rc_channels_values(void);
+static void crsf_store_rc_channels(uint8_t *p_payload, uint8_t len);
 
-static crsf_config_t crsf_config = {};
-
-
-/* prototypes */
-
-static bool crsf_init_config(crsf_config_t *p_config, uint32_t usart);
-static bool crsf_check_packet_crc(void);
-
-
-/* private functions */
-
-static bool crsf_init_config(crsf_config_t *p_config, uint32_t usart)
+static bool crsf_parse_input(void)
 {
-  bool result = false;
+  uint8_t ch = 0;
+  uint8_t packet_len = 0;
+  bool has_valid_crc = false;
+  uint8_t *payload_ptr = NULL;
 
-  if (NULL != p_config)
+  while (true == ring_pop(&rx_ring, &ch))
   {
-    p_config->usart = usart;
-    switch (p_config->usart)
+    if ((0 == crsf_buffer_pos) && (CRSF_SYNC != ch))
     {
-      case USART1:
+      /* skip incoming data until sync byte */
+      continue;
+    }
+    else
+    {
+      /* fill buffer with incoming data */
+      crsf_buffer[crsf_buffer_pos++] = ch;
+      if (CRSF_BUFFER_POS_LEN < crsf_buffer_pos)
       {
-        p_config->gpio_port_rx = GPIO_BANK_USART1_RX;
-        p_config->gpio_port_tx = GPIO_BANK_USART1_TX;
-        p_config->gpio_pin_rx = GPIO_USART1_RX;
-        p_config->gpio_pin_tx = GPIO_USART1_TX;
-        p_config->irq = NVIC_USART1_IRQ;
-        p_config->initialized = true;
-        break;
+        /* data length value received */
+        packet_len = crsf_buffer[CRSF_BUFFER_POS_LEN];
+        if ((packet_len + 2) == crsf_buffer_pos)
+        {
+          /* full packet received */
+          crsf_buffer_pos = 0;
+          has_valid_crc = (crsf_buffer[packet_len + 1] == crc8_calc(&crsf_buffer[CRSF_BUFFER_POS_TYPE], packet_len - 1));
+          if (true == has_valid_crc)
+          {
+            /* packet has valid checksum */
+            payload_ptr = &crsf_buffer[CRSF_BUFFER_POS_PAYLOAD];
+            crsf_parse_packet(crsf_buffer[CRSF_BUFFER_POS_TYPE], payload_ptr, packet_len - 2);
+          }
+        }
       }
+    }
+  }
 
-      case USART2:
-      {
-        p_config->gpio_port_rx = GPIO_BANK_USART2_RX;
-        p_config->gpio_port_tx = GPIO_BANK_USART2_TX;
-        p_config->gpio_pin_rx = GPIO_USART2_RX;
-        p_config->gpio_pin_tx = GPIO_USART2_TX;
-        p_config->irq = NVIC_USART2_IRQ;
-        p_config->initialized = true;
-        break;
-      }
+  return has_valid_crc;
+}
 
-      case USART3:
+static void crsf_parse_packet(uint8_t type, uint8_t *payload, uint8_t len)
+{
+  if (NULL != payload)
+  {
+    switch (type)
+    {
+      case CRSF_TYPE_RC_CHANNELS_PACKED:
       {
-        p_config->gpio_port_rx = GPIO_BANK_USART3_RX;
-        p_config->gpio_port_tx = GPIO_BANK_USART3_TX;
-        p_config->gpio_pin_rx = GPIO_USART3_RX;
-        p_config->gpio_pin_tx = GPIO_USART3_TX;
-        p_config->irq = NVIC_USART3_IRQ;
-        p_config->initialized = true;
+        crsf_store_rc_channels(payload, len);
         break;
       }
 
@@ -180,59 +148,118 @@ static bool crsf_init_config(crsf_config_t *p_config, uint32_t usart)
       }
     }
   }
-
-  return result;
 }
 
-static bool crsf_check_packet_crc(void)
+static void crsf_reset_rc_channels_values(void)
 {
-  uint8_t len = crsf_buf[CRSF_POS_LEN];
-  uint8_t crc = crsf_buf[CRSF_POS_LEN + len];
-  return (crc == crc8_calc(&crsf_buf[CRSF_POS_TYPE], len - 1));
+  uint8_t i = 0;
+
+  for (i = 0; i < 16; i++)
+  { 
+    crsf_rc_channels[i] = 0;
+  }
 }
 
-
-/* public functions */
-
-bool crsf_init(uint32_t usart)
+static void crsf_store_rc_channels(uint8_t *p_payload, uint8_t len)
 {
-  bool result = false;
-
-  if (true == crsf_init_config(&crsf_config, usart))
+  if (NULL != p_payload)
   {
-    /* init ring buffers */
-    ring_init(&crsf_config.rx_ring, rx_buf, sizeof(rx_buf));
-    ring_init(&crsf_config.tx_ring, tx_buf, sizeof(tx_buf));
+    crsf_rc_channels_t *p_rc_channels_packed = (crsf_rc_channels_t *)(p_payload);
+    if (CRSF_LEN_RC_CHANNELS_PACKED == len)
+    {
+      crsf_rc_channels[0]  = p_rc_channels_packed->ch0;
+      crsf_rc_channels[1]  = p_rc_channels_packed->ch1;
+      crsf_rc_channels[2]  = p_rc_channels_packed->ch2;
+      crsf_rc_channels[3]  = p_rc_channels_packed->ch3;
+      crsf_rc_channels[4]  = p_rc_channels_packed->ch4;
+      crsf_rc_channels[5]  = p_rc_channels_packed->ch5;
+      crsf_rc_channels[6]  = p_rc_channels_packed->ch6;
+      crsf_rc_channels[7]  = p_rc_channels_packed->ch7;
+      crsf_rc_channels[8]  = p_rc_channels_packed->ch8;
+      crsf_rc_channels[9]  = p_rc_channels_packed->ch9;
+      crsf_rc_channels[10] = p_rc_channels_packed->ch10;
+      crsf_rc_channels[11] = p_rc_channels_packed->ch11;
+      crsf_rc_channels[12] = p_rc_channels_packed->ch12;
+      crsf_rc_channels[13] = p_rc_channels_packed->ch13;
+      crsf_rc_channels[14] = p_rc_channels_packed->ch14;
+      crsf_rc_channels[15] = p_rc_channels_packed->ch15;
+    }
+  }
+}
 
-    /* init crc8 lookup table */
-    crc8_init(CRSF_CRC_POLY);
+void CRSF_USART_ISR(void)
+{
+  uint8_t ch = 0;
 
-    /* configure usart */
-    gpio_set_mode(crsf_config.gpio_port_rx, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, crsf_config.gpio_pin_rx);
-    gpio_set_mode(crsf_config.gpio_port_tx, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, crsf_config.gpio_pin_tx);
-    usart_set_baudrate(crsf_config.usart, CRSF_BAUDRATE);
-    usart_set_databits(crsf_config.usart, 8);
-    usart_set_parity(crsf_config.usart, USART_PARITY_NONE);
-    usart_set_stopbits(crsf_config.usart, USART_STOPBITS_1);
-    usart_set_mode(crsf_config.usart, USART_MODE_TX_RX);
-    usart_set_flow_control(crsf_config.usart, USART_FLOWCONTROL_NONE);
-    nvic_enable_irq(crsf_config.irq);
-    usart_enable_rx_interrupt(crsf_config.usart);
-    usart_enable(crsf_config.usart);
-
-    result = true;
+  /* RX not empty */
+  if (true == usart_get_flag(CRSF_USART, USART_SR_RXNE))
+  {
+    ch = (uint8_t)usart_recv(CRSF_USART);
+    ring_push(&rx_ring, ch);
   }
 
-  return result;
+  /* TX empty */
+  if (true == usart_get_flag(CRSF_USART, USART_SR_TXE))
+  {
+    if (true == ring_pop(&tx_ring, &ch))
+    {
+      usart_send(CRSF_USART, ch);
+    }
+    else
+    {
+      usart_disable_tx_interrupt(CRSF_USART);
+    }
+  }
+}
+
+void crsf_init(void)
+{
+  /* init ring buffers */
+  ring_init(&rx_ring, rx_buf, sizeof(rx_buf));
+  ring_init(&tx_ring, tx_buf, sizeof(tx_buf));
+
+  /* init crc8 lookup table */
+  crc8_init(CRSF_CRC_POLY);
+
+  /* configure usart */
+  rcc_periph_clock_enable(CRSF_USART_RCC);
+  gpio_set_mode(CRSF_GPIO_PORT_RX, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, CRSF_GPIO_PIN_RX);
+  gpio_set_mode(CRSF_GPIO_PORT_TX, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, CRSF_GPIO_PIN_TX);
+  usart_set_baudrate(CRSF_USART, CRSF_BAUDRATE);
+  usart_set_databits(CRSF_USART, 8);
+  usart_set_parity(CRSF_USART, USART_PARITY_NONE);
+  usart_set_stopbits(CRSF_USART, USART_STOPBITS_1);
+  usart_set_mode(CRSF_USART, USART_MODE_TX_RX);
+  usart_set_flow_control(CRSF_USART, USART_FLOWCONTROL_NONE);
+  nvic_enable_irq(CRSF_USART_IRQ);
+  usart_enable_rx_interrupt(CRSF_USART);
+  usart_enable(CRSF_USART);
+}
+
+void crsf_update(uint32_t timestamp_ms)
+{
+  static uint32_t last_valid_timestamp_ms = 0;
+
+  if (CRSF_FAILSAFE_TIMEOUT_MS < (timestamp_ms - last_valid_timestamp_ms))
+  {
+    crsf_reset_rc_channels_values();
+  }
+
+  if (true == crsf_parse_input())
+  {
+    last_valid_timestamp_ms = timestamp_ms;
+  }
 }
 
 uint16_t crsf_get_channel(uint8_t id)
 {
   uint16_t val = 0;
-  /* TODO */
+
+  if (16 > id)
+  {
+    val = crsf_rc_channels[id];
+  }
+
   return val;
 }
 
-void crsf_update(uint32_t timestamp_ms)
-{
-}
--- crsf.h
+++ crsf.h
@@ -2,10 +2,10 @@
 #define __CRSF_H__
 
 #include <stdint.h>
-#include <stdbool.h>
 
-bool crsf_init(uint32_t usart);
+void crsf_init(void);
 void crsf_update(uint32_t timestamp_ms);
 uint16_t crsf_get_channel(uint8_t id);
 
 #endif /* __CRSF_H__ */
+
